6ae5847eec815665b89d41cb413693d1eb71b8cd
Kensho <ken2021zone@gmail.com>
2021-12-27 23:16:04 +0900

    サイドバーのｊｓまで。

---
+++ js/script.js
@@ -0,0 +1,17 @@
+jQuery( function( $ ) {
+	$( ".js-sidebar" ).on( "click", function() {
+		$( ".p-sidebar" ).toggleClass( "is-open" );
+        $("body").append('<div class="c-menu__bg">');
+        $(".c-menu__bg").fadeIn();
+        $( ".js-sidebar__out" ).on( "click", function() {
+            $(".c-menu__bg").fadeOut(function() {
+                $(this).remove();
+            });
+        });
+
+		// $( ".p-gmenu" ).toggleClass( "is-open" );
+		// $( "body" ).toggleClass( "is-open" );
+        return false;
+	} );
+} );
+
